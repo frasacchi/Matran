5724413efae6e4fef153ab75b799a964c29dd2d7
Fintan Healy <qe19391@bristol.ac.uk>
2021-02-16 11:15:46 +0000

    and pan,zoom and rotate functionalitly to the plot

--- tbx/matran/+mni/+bulk/FEModel.m
+++ tbx/matran/+mni/+bulk/FEModel.m
@@ -270,6 +270,12 @@ classdef FEModel < mni.mixin.Collector
                 ylabel(hAx, 'Y');
                 zlabel(hAx, 'Z');
             end
+            
+            set(hF, 'WindowButtonDownFcn',    @ButtonDownCallback, ...
+                      'WindowScrollWheelFcn',   @WindowScrollWheelCallback, ...
+                      'KeyPressFcn',            @KeyPressCallback, ...
+                      'WindowButtonUpFcn',      @ButtonUpCallback)
+      
             validateattributes(hAx, {'matlab.graphics.axis.Axes'}, {'scalar'}, class(obj), 'hAx');
             
             %Run 'drawElement' method for each bulk object in the model
@@ -284,24 +290,123 @@ classdef FEModel < mni.mixin.Collector
             axis(hAx, 'equal');
             
         end
+        
     end
-    
 end
 
-%Callbacks for UI
-function cbToggleVisible(~, evt)
-%cbToggleVisible Toggles the visibility of a graphic object when it is
-%clicked in the legend.
+function ButtonDownCallback(src, eventdata)
+if strcmp(get(src, 'SelectionType'), 'normal')
+% -> the left mouse button is clicked once
+% enable the interactive rotation
+ppos = get(0, 'PointerLocation');
+set(gca, 'UserData', ppos)
+set(gcf,'WindowButtonMotionFcn',@ButtonMotionCallback)
+ButtonMotionCallback(src)   
+elseif strcmp(get(src, 'SelectionType'), 'extend')
+% -> the left mouse button is clicked once
+% enable the interactive rotation
+ppos = get(0, 'PointerLocation');
+set(gca, 'UserData', ppos)
+set(gcf,'WindowButtonMotionFcn',@ButtonDragCallback)
+ButtonDragCallback(src)
+elseif strcmp(get(src, 'SelectionType'), 'open')
+% -> the left mouse button is double-clicked
+% create a datatip
+cursorMode = datacursormode(src);
+hDatatip = cursorMode.createDatatip(get(gca, 'Children'));
 
-if ~isprop(evt.Peer, 'Visible')
+% move the datatip to the position
+ax_ppos = get(gca, 'CurrentPoint');
+ax_ppos = ax_ppos([1, 3, 5]);  
+% uncomment the next line for Matlab R2014a and earlier
+% set(get(hDatatip, 'DataCursor'), 'DataIndex', index, 'TargetPoint', ax_ppos)
+set(hDatatip, 'Position', ax_ppos)
+cursorMode.updateDataCursors    
+end
+end
+function ButtonMotionCallback(src, eventdata)
+% check if the user data exist
+if isempty(get(gca, 'UserData'))
     return
 end
-
-switch evt.Peer.Visible
-    case 'on'
-        evt.Peer.Visible = 'off';
-    case 'off'
-        evt.Peer.Visible = 'on';
+% camera rotation
+old_ppos = get(gca, 'UserData');
+new_ppos = get(0, 'PointerLocation');
+set(gca, 'UserData', new_ppos)
+dx = (new_ppos(1) - old_ppos(1))*0.25;
+dy = (new_ppos(2) - old_ppos(2))*0.25;
+camorbit(gca, -dx, -dy)
 end
+
+function ButtonDragCallback(src, eventdata)
+% check if the user data exist
+if isempty(get(gca, 'UserData'))
+    return
+end
+% camera rotation
+old_ppos = get(gca, 'UserData');
+new_ppos = get(0, 'PointerLocation');
+set(gca, 'UserData', new_ppos)
+dx = (new_ppos(1) - old_ppos(1))*0.01;
+dy = (new_ppos(2) - old_ppos(2))*0.01;
+camdolly(gca, -dx, -dy, 0)
+end
+
+function WindowScrollWheelCallback(src, eventdata)
+% set the zoom facor
+if eventdata.VerticalScrollCount < 0
+    % increase the magnification
+    zoom_factor = 1.05;
+else 
+    % decrease the magnification
+    zoom_factor = 0.95;
+end
+% camera zoom
+camzoom(zoom_factor)
+end
+function KeyPressCallback(src, eventdata)
+% check which key is pressed
+if strcmp(eventdata.Key, 'uparrow')
+    dx = 0; dy = 0.05; 
+elseif strcmp(eventdata.Key, 'downarrow')
+    dx = 0; dy = -0.05; 
+elseif strcmp(eventdata.Key, 'leftarrow')
+    dx = -0.05; dy = 0;
+elseif strcmp(eventdata.Key, 'rightarrow')
+    dx = 0.05; dy = 0;
+else
+    dx = 0; dy = 0;
+end
+% camera pan
+camdolly(gca, dx, dy, 0)
+% once again check which key is pressed
+if strcmp(eventdata.Key, 'home')
+    % restore the original axes and exit the explorer
+    delete(gca)
+    ax0 = get(src, 'UserData');
+    set(ax0, 'Visible', 'on')
+    set(ax0.Children, 'Visible', 'on')
+    reset(src)
+    explorer(gcf)
+end
+end
+function ButtonUpCallback(src, eventdata)
+% clear the pointer position
+    set(gca, 'UserData', [])
+end
+function cbToggleVisible(~, evt)
+    %cbToggleVisible Toggles the visibility of a graphic object when it is
+    %clicked in the legend.
+
+    if ~isprop(evt.Peer, 'Visible')
+        return
+    end
+
+    switch evt.Peer.Visible
+        case 'on'
+            evt.Peer.Visible = 'off';
+        case 'off'
+            evt.Peer.Visible = 'on';
+    end
 end
 
