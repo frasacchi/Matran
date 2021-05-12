705565df97d2cf1f1864665c147a9abedea2b685
Fintan Healy <qe19391@bristol.ac.uk>
2021-05-12 12:52:38 +0100

    added ability to read matrix from op4 file

---
+++ tbx/matran/+mni/+result/@op4/op4.m
@@ -0,0 +1,26 @@
+classdef op4
+    %OP4 Summary of this class goes here
+    %   Detailed explanation goes here   
+    properties
+        filepath;
+    end
+    
+    methods
+        function obj = op4(filepath)
+            %OP4 Construct an instance of this class
+            %   if filepathhas not been supplied it will open a filedialog
+            
+            if ~exist('filepath','var')
+                [fname, pname, ~] = uigetfile('*.op4',...
+                'Select the op4 output file from a NASTRAN analysis');   
+                filepath = fullfile(pname,fname);
+            end
+            
+            obj.filepath = filepath;
+            if ~isfile(filepath)
+               error('Specified op4 file does not exist');
+            end
+        end
+    end
+end
+
