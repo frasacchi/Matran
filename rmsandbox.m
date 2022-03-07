b318b70a34fd92b979a9d1728ea85fbee75aaa97
Fintan Healy <qe19391@bristol.ac.uk>
2022-03-07 19:04:14 +0000

    Convert sandbox loading to use MATLAB settings
    this stops packages being loaded more than once

---
+++ rmsandbox.m
@@ -0,0 +1,36 @@
+function rmsandbox()
+%rmsandbox  Uninstall sandbox
+%
+%  See also: rmsandbox, 
+%            modify_sandbox_path
+%
+%  Copyright 2016 The MathWorks, Inc.
+%
+% Edits Christopher Szczyglowski, University of Bristol 2020
+%   - Refactored most of the code into 'modify_sandbox_path'
+% Edits Fintan Healy, University of Bristol 2022
+%   - Refactored to use settings so sandboxes aren't loaded multiple times
+
+name = 'Matran';
+s = settings;
+[calling_dir,~,~] = fileparts(mfilename('fullpath'));
+if hasGroup(s,"InstalledSandboxes")
+    if hasGroup(s.InstalledSandboxes,name)
+        installed_dir = s.InstalledSandboxes.(name).dir;
+        if installed_dir ~= calling_dir
+            warning("Package %s is not at the location %s \n so installation will be skipped.\n Itr is installed at @ %s",...
+                name,calling_dir,installed_dir);
+            return
+        end
+    else
+        removeGroup(s.InstalledSandboxes,name);
+    end
+end
+
+sub_directory_to_remove = {'tbx' ; 'unit_testing' ; 'examples'};
+modify_sandbox_path(sub_directory_to_remove, 'remove');
+
+% call submodules
+run(fullfile('modules','Matran','rm_sandbox.m'))
+
+end
