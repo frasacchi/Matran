ec21b6509a298ac11b4de119d966a8266459cd7b
fh9g12 <fintanhealy@live.co.uk>
2023-04-02 12:46:34 +0100

    read bush forces

---
+++ tbx/matran/+mni/+result/@hdf5/read_force_BUSH.m
@@ -0,0 +1,47 @@
+function [res] = read_force_BUSH(obj)
+%read_modal_force_CBEAM Reads the modeshapes from the .h5 file 
+
+% Outputs :
+%   - 'Modeshapes' : Structure containg all of the Modeshape data. 
+%                    Fields include:
+%                       + Mode - Mode number
+%                       + order - Order of the mode
+%                       + eigenvalue - Eigenvalue of the mode
+%                       + radians - Radians of the mode
+%                       + cycles - Frequency of the mode
+%                       + gen_mass - Generalised mass of the mode
+%                       + gen_stiff - Generalised stiffness of the mode
+%                       + EIDs - Element IDs
+%                       + Mx - Moment about the x-axis (at each end of beam)
+%                       + My - Moment about the y-axis (at each end of beam)
+%                       + Mz - Moment about the z-axis (at each end of beam)
+%                       + Fx - Force in the x-axis (at each end of beam)
+%                       + Fy - Force in the y-axis (at each end of beam)
+%                       + Fz - Force in the z-axis (at each end of beam)
+% -------------------------------------------------------------------------
+% Author: Fintan Healy
+% Contact: fintan.healy@bristol.ac.uk
+% Created: 06/03/2023
+% Modified: 06/03/2023
+%
+% Change Log:
+%   - 
+% ======================================================================= %
+
+meta = h5read(obj.filepath,'/INDEX/NASTRAN/RESULT/ELEMENTAL/ELEMENT_FORCE/BUSH');
+data = h5read(obj.filepath,'/NASTRAN/RESULT/ELEMENTAL/ELEMENT_FORCE/BUSH');
+% extract elemental forces
+res = struct();
+for i = 1:length(meta.DOMAIN_ID)
+    idx = data.DOMAIN_ID == meta.DOMAIN_ID(i);
+    res(i).EIDs = data.EID(idx);
+%     res(i).GIDs = data.GRID(idx);
+    res(i).Fx = data.FX(idx);
+    res(i).Fy = data.FY(idx);
+    res(i).Fz = data.FZ(idx);
+    res(i).Mx = data.MX(idx);
+    res(i).My = data.MY(idx);
+    res(i).Mz = data.MZ(idx);
+end
+end
+
